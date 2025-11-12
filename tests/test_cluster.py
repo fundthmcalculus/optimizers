f9d727f3cfc1d52cdd5c54ca640e718c332d784e
Scott Phillips <polygonguru@gmail.com>
2025-11-12 13:45:18 -0500

    fix: in-place VAT (#55)
    * fix: in-place VAT
    
    * black formatting

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -3,19 +3,61 @@ import time
 import numpy as np
 from cluster import compute_ordered_dis_njit_merge
 from matplotlib import pyplot as plt
-from pyclustertend.visual_assessment_of_tendency import compute_ordered_dis_njit
+
+# from pyclustertend.visual_assessment_of_tendency import compute_ordered_dis_njit
 from sklearn.metrics import pairwise_distances
+from PIL import Image
 
 from test_combinatorics import circle_random_clusters
+
+
+def test_cluster_uci_letter_reco():
+    print("\n")
+    from ucimlrepo import fetch_ucirepo
+
+    # fetch dataset
+    # 59 is letter recognition
+    # 827 is sepsis survival (allocates 80+ GB RAM)
+    # 148 is shuttle stat log (allocates 50 GB RAM)
+    letter_recognition = fetch_ucirepo(id=148)
+
+    # data (as pandas dataframes)
+    X = letter_recognition.data.features
+    y = letter_recognition.data.targets
+
+    # metadata
+    print(f"Metadata: {letter_recognition.metadata}")
+
+    # variable information
+    print(f"Variable Information: {letter_recognition.variables}")
+
+    # Compute the pairwise distances
+    matrix_of_pairwise_distance = np.log(pairwise_distances(X))
+    matrix_of_pairwise_distance = (
+        matrix_of_pairwise_distance / matrix_of_pairwise_distance.max()
+    )
+    print(f"Pairwise distance matrix shape: {matrix_of_pairwise_distance.shape}")
+    t0 = time.time()
+    ordered_matrix, path_merge = compute_ordered_dis_njit_merge(
+        matrix_of_pairwise_distance, inplace=True
+    )
+    t1 = time.time()
+
+    print(f"Elapsed time for {len(X)} data points: {t1-t0:.02f}")
+
+    # Save the ordered matrix as an image
+    # img_array = (ordered_matrix * 255).astype(np.uint8)
+    # img = Image.fromarray(img_array)
+    # img.save('ordered_matrix.png')
 
 
 def test_vat_scaling():
     city_count: list[int] = []
     merge_time: list[float] = []
     lib_time: list[float] = []
-    o1 = 3
-    o2 = 10
-    n = 5
+    o1 = 2
+    o2 = 9
+    n = 8
     for group_count in np.logspace(o1, o2, n, base=2, dtype="int"):
         city_count.append(group_count)
         # print(f"City count: {group_count}")
@@ -30,12 +72,12 @@ def test_vat_scaling():
         # Cluster using our IVAT
         t0 = time.time()
         ordered_matrix2, path_merge = compute_ordered_dis_njit_merge(
-            matrix_of_pairwise_distance
+            matrix_of_pairwise_distance, inplace=True
         )
         t1 = time.time()
         # Cluster using the library IVAT
-        ordered_matrix = compute_ordered_dis_njit(matrix_of_pairwise_distance)
-        # ordered_matrix = 0*ordered_matrix2
+        # ordered_matrix = compute_ordered_dis_njit(matrix_of_pairwise_distance.copy())
+        ordered_matrix = 0 * ordered_matrix2
         t2 = time.time()
 
         # Print the results
@@ -62,10 +104,10 @@ def test_vat_scaling():
 
     # Plot the results
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(4, 10))
-    fig.suptitle(f"VAT Scaling Test (N={group_count})")
-    im1 = ax1.imshow(ordered_matrix, cmap="viridis")
-    ax1.set_title(f"Library VAT Result: {t2-t1:.2f} seconds")
-    plt.colorbar(im1, ax=ax1)
+    # fig.suptitle(f"VAT Scaling Test (N={group_count})")
+    # im1 = ax1.imshow(ordered_matrix, cmap="viridis")
+    # ax1.set_title(f"Library VAT Result: {t2-t1:.2f} seconds")
+    # plt.colorbar(im1, ax=ax1)
 
     im2 = ax2.imshow(ordered_matrix2, cmap="viridis")
     ax2.set_title(f"Merge Sort VAT Result: {t1-t0:.2f} seconds")
